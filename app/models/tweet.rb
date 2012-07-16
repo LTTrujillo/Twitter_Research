ec36eef8741c909fd0ed2e5e4b71281c72b9b76c
Logan Trujillo <logantrujillo7767@gmail.com>
2012-07-16 12:01:23 -0600

    Added the tweet.rb model

---
+++ app/models/tweet.rb
@@ -0,0 +1,13 @@
+class Tweet < ActiveRecord::Base
+
+  attr_accessible :twitter_user, :tweeted_text, :tweeted_text, :user_id
+  validates(:twitter_user, :presence => true)
+  validates(:tweeted_text, :presence => true)
+  validates(:tweeted_at, :presence => true)
+  validates(:user_id, :presence => true)
+  belongs_to(:user)
+
+end
+
+
+
